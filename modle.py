d698867a6f7b0369d1ce43cd1348a0b116c417e3
baftcnet <baftcnet@163.com>
2026-05-03 19:33:42 +0700

    Update modle.py

--- modle.py
+++ modle.py
@@ -392,8 +392,8 @@ class EEGNet(nn.Module):
         )
         self.drop_temp = nn.Dropout(p=dropout_temp)        
         self.region_channels = {
-            'Frontal Region': [0,1,2,3,4,5,6,7,8,9,10,11,12],  
-            'Central Region': [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
+            'Frontal Region': [0,1,2,3,4,5,6,7,8,9,10,11,12],  #MA
+            'Central Region': [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],#SA
             'zong': [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
         }
         self.num_regions = len(self.region_channels)
